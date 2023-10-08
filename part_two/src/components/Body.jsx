6fef9008a721c412ba80eb4a6f251267f9b07f57
Pranay <pranay.r22@gmail.com>
2023-10-08 09:46:51 -0700

    Add part_two contents

---
+++ part_two/src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from "react";  
+
+function Body() {
+    return(
+        <body>
+            <div className="note">
+                <h1>This is a note title</h1>
+                <p>This is the note content</p>
+            </div>
+        </body>
+        )
+}
+
+export default Body;
